cfa2a2e79a22901e20a40e05469bacdfdb42b635
Demis Rincon <darmfma@gmail.com>
2024-10-08 12:50:44 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  height: 100vh;
+  background-color: #282c34;
+  color: white;
+  background-color: #282c34;
+  min-height: 100vh;
+  height: 100%;
+`;
+const Header = styled.header`
+  background-color: white;
+  height: 60px;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  height: 90%;
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+  flex-direction: column;
+`;
+
+
+
+
+
+const Pkmn = ({name}) => {
+  return <div>{name}</div>;
+};
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results);
+        setPokemon(data.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <Header>
+        <Image
+          src="https://cdn.worldvectorlogo.com/logos/pokemon-23.svg"
+          alt="google logo"
+        />
+      </Header>
+      <Body>
+        {pokemon.map((pkmn, index) => {
+          return<Pkmn key={index} name={pkmn.name} />
+        }
+        )}
+      </Body>
+    </MainContainer>
   );
 }
 
